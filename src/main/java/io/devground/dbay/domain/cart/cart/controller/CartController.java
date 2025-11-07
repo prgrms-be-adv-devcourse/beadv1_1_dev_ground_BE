5cb0f187adce77cf49b80f54aa3d06c83af1c924
ljs981026 <ljs981026@naver.com>
2025-11-07 14:58:22 +0900

    [FEAT]Cart_도메인_엔티티_추가

---
+++ src/main/java/io/devground/dbay/domain/cart/cart/controller/CartController.java
@@ -0,0 +1,12 @@
+package io.devground.dbay.domain.cart.cart.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/cart")
+public class CartController {
+}
