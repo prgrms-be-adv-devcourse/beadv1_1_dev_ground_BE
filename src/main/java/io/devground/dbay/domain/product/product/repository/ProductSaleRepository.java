4a4ccd5a00115b463dc21d55625bbcb8d8726f68
Gyeonghun Min <riverpower6@naver.com>
2025-11-10 17:10:28 +0900

    [TEST] Product 판매 정보 수정 테스트코드 구현

--- src/main/java/io/devground/dbay/domain/product/product/repository/ProductSaleRepository.java
+++ src/main/java/io/devground/dbay/domain/product/product/repository/ProductSaleRepository.java
@@ -1,8 +1,12 @@
 package io.devground.dbay.domain.product.product.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.devground.dbay.domain.product.product.entity.ProductSale;
 
 public interface ProductSaleRepository extends JpaRepository<ProductSale, Long> {
+
+	Optional<ProductSale> findByCode(String code);
 }
