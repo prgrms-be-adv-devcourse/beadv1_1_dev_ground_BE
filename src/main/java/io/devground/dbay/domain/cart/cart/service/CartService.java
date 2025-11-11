7ae7ad055db04b30c12c088cb2deb14b20bd9f65
ljs981026 <ljs981026@naver.com>
2025-11-11 11:40:11 +0900

    [FEAT] 장바구니 상품 삭제 구현

--- src/main/java/io/devground/dbay/domain/cart/cart/service/CartService.java
+++ src/main/java/io/devground/dbay/domain/cart/cart/service/CartService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import io.devground.dbay.domain.cart.cart.model.entity.Cart;
 import io.devground.dbay.domain.cart.cart.model.vo.AddCartItemRequest;
+import io.devground.dbay.domain.cart.cart.model.vo.DeleteItemsByCartRequest;
 import io.devground.dbay.domain.cart.cart.model.vo.GetItemsByCartResponse;
 import io.devground.dbay.domain.cart.cartItem.model.entity.CartItem;
 
@@ -13,4 +14,6 @@ public interface CartService {
 	CartItem addItem(String cartCode, AddCartItemRequest request);
 
 	GetItemsByCartResponse getItemsByCart(String cartCode);
+
+	int deleteItemsByCart(String cartCode, DeleteItemsByCartRequest request);
 }
--- src/test/java/io/devground/dbay/domain/cart/service/CartServiceImplTest.java
+++ src/test/java/io/devground/dbay/domain/cart/service/CartServiceImplTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,6 +22,7 @@ import io.devground.dbay.domain.cart.cart.model.entity.Cart;
 import io.devground.dbay.domain.cart.cart.model.vo.AddCartItemRequest;
 import io.devground.dbay.domain.cart.cart.model.vo.AddCartItemResponse;
 import io.devground.dbay.domain.cart.cart.model.vo.CartProductListResponse;
+import io.devground.dbay.domain.cart.cart.model.vo.DeleteItemsByCartRequest;
 import io.devground.dbay.domain.cart.cart.model.vo.GetItemsByCartResponse;
 import io.devground.dbay.domain.cart.cart.repository.CartRepository;
 import io.devground.dbay.domain.cart.cart.service.CartServiceImpl;
@@ -312,5 +314,89 @@ class CartServiceImplTest {
 			});
 
 		verifyNoInteractions(productFeignClient);
+	}
+
+	@Test
+	@DisplayName("성공_장바구니 상품 삭제")
+	void deleteItemsByCart_success() {
+		String cartCode = UUID.randomUUID().toString();
+		List<String> cartProductCodes = List.of("p1", "p2", "p3");
+
+		given(cartRepository.findByCode(cartCode))
+			.willReturn(Optional.of(mock(Cart.class)));
+		given(cartItemRepository.deleteCartItemByProductCodes(cartCode, cartProductCodes))
+			.willReturn(cartProductCodes.size());
+
+		int result = cartService.deleteItemsByCart(cartCode, new DeleteItemsByCartRequest(cartProductCodes));
+
+		assertThat(result).isEqualTo(cartProductCodes.size());
+		then(cartItemRepository).should().deleteCartItemByProductCodes(cartCode, cartProductCodes);
+	}
+
+	@Test
+	@DisplayName("실패_장바구니 삭제에서 코드 유효한 값")
+	void deleteItemsByCart_throwException_whenInvalidCode() {
+		String cartCode = "invalid code";
+
+		assertThatThrownBy(() -> cartService.deleteItemsByCart(cartCode, new DeleteItemsByCartRequest(List.of())))
+			.isInstanceOf(ServiceException.class)
+			.satisfies(ex -> {
+				ServiceException se = (ServiceException) ex;
+				assertThat(se.getErrorCode().getMessage()).isEqualTo("잘못된 코드 형식입니다.");
+			});
+
+		verifyNoInteractions(cartRepository, cartItemRepository);
+	}
+
+	@Test
+	@DisplayName("실패_장바구니 삭제 상품 목록 빈 값")
+	void deleteItemsByCart_throwException_whenCartProductCodeEmpty() {
+		String cartCode = UUID.randomUUID().toString();
+
+		assertThatThrownBy(() -> cartService.deleteItemsByCart(cartCode, new DeleteItemsByCartRequest(List.of())))
+			.isInstanceOf(ServiceException.class)
+			.satisfies(ex -> {
+				ServiceException se = (ServiceException) ex;
+				assertThat(se.getErrorCode().getMessage()).isEqualTo("삭제할 상품이 선택되지 않았습니다.");
+			});
+
+		verifyNoInteractions(cartRepository, cartItemRepository);
+	}
+
+	@Test
+	@DisplayName("실패_장바구니 삭제에서 장바구니를 찾을 수 없음")
+	void deleteItemsByCart_thrownException_whenCartNotExists() {
+		String cartCode = UUID.randomUUID().toString();
+
+		given(cartRepository.findByCode(cartCode)).willReturn(Optional.empty());
+
+		assertThatThrownBy(
+			() -> cartService.deleteItemsByCart(cartCode, new DeleteItemsByCartRequest(List.of("p1", "p2"))))
+			.isInstanceOf(ServiceException.class)
+			.satisfies(ex -> {
+				ServiceException se = (ServiceException) ex;
+				assertThat(se.getErrorCode().getMessage()).isEqualTo("장바구니를 찾을 수 없습니다.");
+			});
+
+		verifyNoInteractions(cartItemRepository);
+	}
+
+	@Test
+	@DisplayName("실패_장바구니 삭제 결과 수 != 요청 수")
+	void deleteItemsByCart_thrownException_failed() {
+		String cartCode = UUID.randomUUID().toString();
+		DeleteItemsByCartRequest request = new DeleteItemsByCartRequest(List.of("p1", "p2"));
+
+		given(cartRepository.findByCode(cartCode))
+			.willReturn(Optional.of(mock(Cart.class)));
+		given(cartItemRepository.deleteCartItemByProductCodes(cartCode, request.cartProductCodes()))
+			.willReturn(1);
+
+		assertThatThrownBy(() -> cartService.deleteItemsByCart(cartCode, request))
+			.isInstanceOf(ServiceException.class)
+			.satisfies(ex -> {
+				ServiceException se = (ServiceException) ex;
+				assertThat(se.getErrorCode().getMessage()).isEqualTo("장바구니 상품 삭제를 실패했습니다.");
+			});
 	}
 }
