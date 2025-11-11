4f56a8d0875cd2b71f453b7551dae5f0dac2b9a8
kurt <qodydgus97@kakao.com>
2025-11-11 10:11:31 +0900

    [FEAT] Deposit controller 테스트 코드 작성

--- src/main/java/io/devground/dbay/domain/deposit/repository/DepositHistoryJpaRepository.java
+++ src/main/java/io/devground/dbay/domain/deposit/repository/DepositHistoryJpaRepository.java
@@ -1,9 +1,16 @@
 package io.devground.dbay.domain.deposit.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import io.devground.dbay.domain.deposit.entity.Deposit;
+import io.devground.dbay.domain.deposit.entity.DepositHistory;
 
-public interface DepositHistoryJpaRepository extends JpaRepository<Deposit, Long> {
+public interface DepositHistoryJpaRepository extends JpaRepository<DepositHistory, Long> {
 
+	Page<DepositHistory> findByUserCode(String depositCode, Pageable pageable);
+
+	Optional<DepositHistory> findByCode(String historyCode);
 }
