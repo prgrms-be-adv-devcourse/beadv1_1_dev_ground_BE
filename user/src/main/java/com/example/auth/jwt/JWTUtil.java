a9815eccb72220b2e0a6aedfc52094c3d22f7cf5
niki8533 <niki8533@naver.com>
2025-11-11 12:35:37 +0900

    [FEAT] 로그인 기능 구현

---
+++ user/src/main/java/com/example/auth/jwt/JWTUtil.java
@@ -0,0 +1,54 @@
+package com.example.auth.jwt;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JWTUtil {
+	private SecretKey secretKey;
+
+	public JWTUtil(@Value("${custom.jwt.secrets.app-key}") String secret) {
+		this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+	}
+
+	public String getUserCode(String token){
+		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("userCode", String.class);
+	}
+
+	public String getRole(String token) {
+		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+	}
+
+	public Date getExpiration(String token) {
+		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration();
+	}
+
+	public Boolean isExpired(String token) {
+		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+	}
+
+	//토큰 종류
+	public String getCategory(String token) {
+		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
+	}
+
+	public String createJwt(String category, String userCode, String role, Long expiredMs) {
+		return Jwts.builder()
+			.claim("category", category)
+			.claim("userCode", userCode)
+			.claim("role", role)
+			.issuedAt(new Date(System.currentTimeMillis()))
+			.expiration(new Date(System.currentTimeMillis() + expiredMs))
+			.signWith(secretKey)
+			.compact();
+	}
+}
