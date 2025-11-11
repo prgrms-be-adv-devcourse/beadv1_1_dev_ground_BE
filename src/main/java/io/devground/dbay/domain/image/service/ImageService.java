ca071fb9f803655f181c5caf455a573e0e5a356d
Gyeonghun Min <riverpower6@naver.com>
2025-11-11 12:36:17 +0900

    [FEAT] Image 처리 로직 추가

--- src/main/java/io/devground/dbay/domain/image/service/ImageService.java
+++ src/main/java/io/devground/dbay/domain/image/service/ImageService.java
@@ -1,4 +1,18 @@
 package io.devground.dbay.domain.image.service;
 
+import java.util.List;
+
+import io.devground.dbay.domain.image.vo.ImageType;
+
 public interface ImageService {
+
+	void saveImages(ImageType imageType, String referenceCode, List<String> imageUrls);
+
+	void saveImage(ImageType imageType, String referenceCode, String url);
+
+	String getImageByCode(ImageType imageType, String referenceCode);
+
+	void deleteImageByReferences(ImageType imageType, String referenceCode);
+
+	void deleteImagesByReferencesAndUrl(ImageType imageType, String referenceCode, String url);
 }
