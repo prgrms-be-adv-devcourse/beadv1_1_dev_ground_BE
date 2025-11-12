1e2b682238d3b47646764d62591339a4fe78a258
Gyeonghun Min <riverpower6@naver.com>
2025-11-12 12:44:45 +0900

    [FEAT] BaseEntity 메서드 간결화 및 메서드 추가

--- core/src/main/java/io/devground/core/model/web/BaseResponse.java
+++ core/src/main/java/io/devground/core/model/web/BaseResponse.java
@@ -1,5 +1,7 @@
 package io.devground.core.model.web;
 
+import io.devground.core.model.exception.ServiceException;
+import io.devground.core.model.vo.ErrorCode;
 import lombok.NonNull;
 
 public record BaseResponse<T>(
@@ -15,11 +17,31 @@ public record BaseResponse<T>(
 		return new BaseResponse<>(resultCode, msg, data);
 	}
 
+	public static <T> BaseResponse<T> success(int resultCode, T data) {
+		return success(resultCode, data, "");
+	}
+
 	public static <T> BaseResponse<T> success(int resultCode, String msg) {
-		return new BaseResponse<>(resultCode, msg, null);
+		return success(resultCode, null, msg);
 	}
 
 	public static <T> BaseResponse<T> fail(int resultCode, String msg) {
 		return new BaseResponse<>(resultCode, msg, null);
+	}
+
+	public boolean isSuccess() {
+
+		return resultCode >= 200 && resultCode < 400;
+	}
+
+	public BaseResponse<T> throwIfNotSuccess() {
+
+		ErrorCode errorCode = ErrorCode.fromHttpStatus(resultCode, msg);
+
+		if (!isSuccess()) {
+			throw new ServiceException(errorCode);
+		}
+
+		return this;
 	}
 }
