8f69fd1a8f3335efe68cd6a2e331443102d5aa97
Gyeonghun Min <riverpower6@naver.com>
2025-11-16 17:39:13 +0900

    [REFACTOR] 타임아웃 사가 보상 처리 보강 및 관련 Saga 조회 쿼리 최적화

--- src/main/java/io/devground/dbay/domain/product/product/service/ImageScheduledProcessor.java
+++ src/main/java/io/devground/dbay/domain/product/product/service/ImageScheduledProcessor.java
@@ -7,35 +7,39 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import io.devground.core.model.vo.ImageType;
 import io.devground.dbay.common.saga.entity.Saga;
 import io.devground.dbay.common.saga.repository.SagaRepository;
 import io.devground.dbay.common.saga.service.SagaService;
 import io.devground.dbay.common.saga.vo.SagaStatus;
 import io.devground.dbay.common.saga.vo.SagaStep;
-import io.devground.dbay.domain.product.product.client.ImageClient;
-import io.devground.dbay.domain.product.product.mapper.ProductMapper;
+import io.devground.dbay.domain.product.product.infra.kafka.ProductImageSagaOrchestrator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 // TODO: 실행 Term 전략 세우기
 @Slf4j
 @Component
+@Transactional
 @RequiredArgsConstructor
 public class ImageScheduledProcessor {
 
+	private final ProductImageSagaOrchestrator productOrchestrator;
 	private final SagaRepository sagaRepository;
 	private final SagaService sagaService;
-	private final ImageClient imageClient;
 
+	/**
+	 * Timeout된 Saga 보상 처리
+	 * 현재는 5분마다 실행
+	 */
 	@Scheduled(fixedDelay = 300000)
-	@Transactional
 	public void checkTimeoutSagas() {
 
 		LocalDateTime deadline = LocalDateTime.now().minusMinutes(10);
 
-		List<Saga> timeoutSagas =
-			sagaRepository.findSagaByCurrentStepAndStartedAtBefore(SagaStep.WAITING_S3_UPLOAD, deadline);
+		List<Saga> timeoutSagas = sagaRepository.findSagaByCurrentStepInAndStartedAtBefore(
+			List.of(SagaStep.WAITING_S3_UPLOAD, SagaStep.IMAGE_KAFKA_PUBLISHED),
+			deadline
+		);
 
 		if (timeoutSagas.isEmpty()) {
 			return;
@@ -43,7 +47,12 @@ public class ImageScheduledProcessor {
 
 		for (Saga timeoutSaga : timeoutSagas) {
 			try {
-				this.compensateTimeoutSaga(timeoutSaga);
+				log.warn("Timeout된 Saga 처리 시작 - SagaId: {}, ProductCode: {}, Step: {}",
+					timeoutSaga.getSagaId(), timeoutSaga.getReferenceCode(), timeoutSaga.getCurrentStep());
+
+				productOrchestrator.compensateProductImageUploadFailure(
+					timeoutSaga.getSagaId(), timeoutSaga.getReferenceCode(), "Saga Timeout"
+				);
 			} catch (Exception e) {
 				log.error("Timeout된 Saga 처리 실패 - SagaId: {}, ProductCode: {}, Exception: {}",
 					timeoutSaga.getSagaId(), timeoutSaga.getReferenceCode(), e.getMessage());
@@ -51,8 +60,11 @@ public class ImageScheduledProcessor {
 		}
 	}
 
+	/**
+	 * 보상 처리 중 체류된 Saga 정리
+	 * 현재는 10분마다 실행
+	 */
 	@Scheduled(fixedDelay = 600000)
-	@Transactional
 	public void deleteSagasStayedInCompensating() {
 
 		LocalDateTime deadline = LocalDateTime.now().minusMinutes(30);
@@ -69,35 +81,6 @@ public class ImageScheduledProcessor {
 				saga.getSagaId(), saga.getReferenceCode(), saga.getLastErrorMessage());
 
 			sagaService.updateToFail(saga.getSagaId(), "미보상 상태로 멈춘 Saga/수동 처리 필요: " + saga.getLastErrorMessage());
-		}
-	}
-
-	private void compensateTimeoutSaga(Saga saga) {
-
-		String sagaId = saga.getSagaId();
-		String productCode = saga.getReferenceCode();
-
-		try {
-			sagaService.updateToCompensating(sagaId);
-
-			imageClient.compensateUpload(ProductMapper.toDeleteImagesRequest(ImageType.PRODUCT, productCode))
-				.throwIfNotSuccess();
-
-			sagaService.updateToCompensated(sagaId, "Timeout된 Saga 보상 처리 완료");
-
-			log.info("Timeout된 Saga 보상 처리 완료 - SagaId: {}, ProductCode: {}", sagaId, productCode);
-		} catch (Exception e) {
-			log.error("Timeout된 Saga 보상 처리 실패 - SagaId: {}, ProductCode: {}, Exception: {}",
-				sagaId, productCode, e.getMessage()
-			);
-
-			sagaService.updateToFail(
-				sagaId,
-				String.format("Timeout된 Saga 보상 실패/수동 처리 필요: ProductCode: {}, Exception: {}",
-					productCode, e.getMessage())
-			);
-
-			throw e;
 		}
 	}
 }
