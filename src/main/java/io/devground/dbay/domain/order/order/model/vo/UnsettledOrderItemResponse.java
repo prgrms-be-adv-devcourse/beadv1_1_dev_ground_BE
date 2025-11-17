ec52b8d8e7773af901542794eb6ab760143c35ed
ljs981026 <ljs981026@naver.com>
2025-11-17 12:32:42 +0900

    [REFACTOR] cartItemRepository에서 JPQL 사용하여 한번에 조회로 변경

--- src/main/java/io/devground/dbay/domain/order/order/model/vo/UnsettledOrderItemResponse.java
+++ src/main/java/io/devground/dbay/domain/order/order/model/vo/UnsettledOrderItemResponse.java
@@ -1,10 +1,10 @@
 package io.devground.dbay.domain.order.order.model.vo;
 
-import java.util.List;
-
 public record UnsettledOrderItemResponse(
 	String orderCode,
-	String buyerCode,
-	List<UnsettledOrderItemInfo> orderItems
+	String userCode,
+	String orderItemCode,
+	String sellerCode,
+	Long productPrice
 ) {
 }
