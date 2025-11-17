76128202bf42c13de209c2fbebd24d5de193b792
ljs981026 <ljs981026@naver.com>
2025-11-17 17:38:01 +0900

    [REFACTOR] Order 카프카 설정 변경 및 장바구니 카프카 이벤트 핸들러 수정

--- core/src/main/java/io/devground/core/event/order/Temp/event/PaymentCreatedEvent.java
+++ core/src/main/java/io/devground/core/event/order/Temp/event/PaymentCreatedEvent.java
@@ -1,11 +1,14 @@
 package io.devground.core.event.order.Temp.event;
 
+import java.util.List;
+
 import io.devground.core.model.vo.DepositHistoryType;
 
 public record PaymentCreatedEvent(
 	String userCode,
 	Long amount,
 	DepositHistoryType type,
-	String orderCode
+	String orderCode,
+	List<String> productCodes
 ) {
 }
