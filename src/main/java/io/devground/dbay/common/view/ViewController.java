4ce217d8d86a5db80dc125106bcc24ef95bccbb6
niki8533 <niki8533@naver.com>
2025-11-19 01:32:32 +0900

    [FIX] view controller 코드 주석처리

--- src/main/java/io/devground/dbay/common/view/ViewController.java
+++ src/main/java/io/devground/dbay/common/view/ViewController.java
@@ -1,9 +1,6 @@
 package io.devground.dbay.common.view;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.devground.dbay.domain.payment.service.PaymentService;
