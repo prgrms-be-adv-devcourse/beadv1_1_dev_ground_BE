22fdb0bdbffc35a31308ea44b3a9564d142d3db7
Gyeonghun Min <riverpower6@naver.com>
2025-11-19 04:47:17 +0900

    [FIX] SpringDocConfig의 @Configurable을 @Configuration으로 수정

--- src/main/java/io/devground/dbay/springdoc/SpringDocConfig.java
+++ src/main/java/io/devground/dbay/springdoc/SpringDocConfig.java
@@ -1,13 +1,13 @@
 package io.devground.dbay.springdoc;
 
 import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@Configurable
+@Configuration
 @OpenAPIDefinition(info = @Info(title = "D-Bay API"))
 public class SpringDocConfig {
 
