e71ec25ccb9d679c1833281bd9cdf18380201833
jiyu <han01031@naver.com>
2025-11-22 17:26:10 +0900

    [CHORE] infra 코드 수정

--- src/main/java/io/devground/dbay/DbayApplication.java
+++ src/main/java/io/devground/dbay/DbayApplication.java
@@ -1,17 +1,28 @@
 package io.devground.dbay;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/dbay", description = "Dbay service via gateway")
+		}
+)
 @EnableAsync
 @EnableKafka
 @EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "io.devground.dbay")
+@EnableElasticsearchRepositories(basePackages = "io.devground.dbay.domain.product.product.repository")
 public class DbayApplication {
 
 	public static void main(String[] args) {
--- user/src/main/java/com/example/UserApplication.java
+++ user/src/main/java/com/example/UserApplication.java
@@ -1,9 +1,16 @@
 package com.example;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/user", description = "User service via gateway")
+		}
+)
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserApplication {
