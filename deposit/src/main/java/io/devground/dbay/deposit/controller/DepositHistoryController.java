7c07fab3c9e5e7358d4e4650630a65facb4b117e
kurt <qodydgus97@kakao.com>
2025-11-26 17:36:14 +0900

    [REFACTOR] Deposit MSA모듈 분리

--- deposit/src/main/java/io/devground/dbay/deposit/controller/DepositHistoryController.java
+++ deposit/src/main/java/io/devground/dbay/deposit/controller/DepositHistoryController.java
@@ -1,4 +1,4 @@
-package io.devground.dbay.deposit.controller;
+package io.devground.deposit.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
--- deposit/src/test/java/io/devground/deposit/DepositApplicationTests.java
+++ deposit/src/test/java/io/devground/deposit/DepositApplicationTests.java
@@ -2,8 +2,14 @@ package io.devground.deposit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+<<<<<<< HEAD
 import org.springframework.test.context.ActiveProfiles;
 
+=======
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+>>>>>>> cfbedd3 ([REFACTOR] Deposit MSA모듈 분리)
 @SpringBootTest
 class DepositApplicationTests {
 
