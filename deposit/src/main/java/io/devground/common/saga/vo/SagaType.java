25be6ddc1870772e20bd72993cfe1993f5c37ccf
kurt <qodydgus97@kakao.com>
2025-11-27 09:54:09 +0900

    [REFACTOR] Deposit MSA모듈 분리

---
+++ deposit/src/main/java/io/devground/common/saga/vo/SagaType.java
@@ -0,0 +1,14 @@
+package io.devground.common.saga.vo;
+
+import lombok.Getter;
+
+@Getter
+public enum SagaType {
+
+	PRODUCT_IMAGE_REGIST,
+	PRODUCT_IMAGE_UPDATE,
+	PRODUCT_IMAGE_DELETE,
+
+	// Settlement
+	SETTLEMENT_DEPOSIT_CHARGE
+}
