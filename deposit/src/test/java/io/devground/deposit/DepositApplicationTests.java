cfe668f06806ad89bd8e25da641472b98a119204
kurt <qodydgus97@kakao.com>
2025-11-27 10:34:03 +0900

    [REFACTOR] boot 버전 변경

--- deposit/src/test/java/io/devground/deposit/DepositApplicationTests.java
+++ deposit/src/test/java/io/devground/deposit/DepositApplicationTests.java
@@ -2,9 +2,8 @@ package io.devground.deposit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class DepositApplicationTests {
 
