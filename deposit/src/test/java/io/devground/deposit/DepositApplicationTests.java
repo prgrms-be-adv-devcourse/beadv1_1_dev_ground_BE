8368e91e0bc2a6982e2335df07714101d4344a23
niki8533 <niki8533@naver.com>
2025-11-28 09:32:54 +0900

    [REFACTOR] user yml 파일 변경

---
+++ deposit/src/test/java/io/devground/deposit/DepositApplicationTests.java
@@ -0,0 +1,14 @@
+package io.devground.deposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class DepositApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
