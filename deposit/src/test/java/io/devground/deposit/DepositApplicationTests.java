e2bbbaf8b122750a93d8348ee5bfd7c98791d5ce
niki8533 <niki8533@naver.com>
2025-11-28 09:32:54 +0900

    [REFACTOR] user yml 파일 변경

--- deposit/src/test/java/io/devground/deposit/DepositApplicationTests.java
+++ deposit/src/test/java/io/devground/deposit/DepositApplicationTests.java
@@ -2,19 +2,8 @@ package io.devground.deposit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-<<<<<<< HEAD
-<<<<<<< HEAD
 import org.springframework.test.context.ActiveProfiles;
 
-=======
-import org.springframework.context.annotation.Import;
-
-@Import(TestcontainersConfiguration.class)
->>>>>>> cfbedd3 ([REFACTOR] Deposit MSA모듈 분리)
-=======
-import org.springframework.test.context.ActiveProfiles;
-
->>>>>>> f947721 ([REFACTOR] boot 버전 변경)
 @SpringBootTest
 class DepositApplicationTests {
 
