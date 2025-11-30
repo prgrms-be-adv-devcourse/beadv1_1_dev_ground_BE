b3860cef546217ed7089e56e172567a78d5ca99d
Gyeonghun Min <riverpower6@naver.com>
2025-11-30 18:20:19 +0900

    [FIX] 운영 환경에서의 Swagger 버그 수정

--- user/src/main/java/com/example/UserApplication.java
+++ user/src/main/java/com/example/UserApplication.java
@@ -1,16 +1,14 @@
 package com.example;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@OpenAPIDefinition(
-		servers = {
-				@Server(url = "/user", description = "User service via gateway")
-		}
-)
+// @OpenAPIDefinition(
+// 		servers = {
+// 				@Server(url = "/user", description = "User service via gateway")
+// 		}
+// )
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserApplication {
