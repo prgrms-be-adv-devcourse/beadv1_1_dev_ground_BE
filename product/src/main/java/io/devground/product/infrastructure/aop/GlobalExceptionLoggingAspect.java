3c4026db653cc8a3209e9988a5d27f600c8e57e3
GyeongHun <riverpower6@naver.com>
2025-12-01 17:26:51 +0900

    [FEAT] 기존 common에 구현해둔 aop, config, util, logback 이식

---
+++ product/src/main/java/io/devground/product/infrastructure/aop/GlobalExceptionLoggingAspect.java
@@ -0,0 +1,20 @@
+package io.devground.product.infrastructure.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import io.devground.product.infrastructure.util.LogUtil;
+import lombok.RequiredArgsConstructor;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class GlobalExceptionLoggingAspect {
+
+	@AfterThrowing(pointcut = "@within(org.springframework.stereotype.Service)", throwing = "ex")
+	public void logGlobalException(JoinPoint joinPoint, Throwable ex) {
+		LogUtil.logError(joinPoint, ex);
+	}
+}
