4bef07fb82fa4b5e8b3e880cfdbed2b352bf2adb
GyeongHun <riverpower6@naver.com>
2025-12-01 17:26:51 +0900

    [FEAT] 기존 common에 구현해둔 aop, config, util, logback 이식

---
+++ product/src/main/java/io/devground/product/infrastructure/util/LogUtil.java
@@ -0,0 +1,74 @@
+package io.devground.product.infrastructure.util;
+
+import org.aspectj.lang.JoinPoint;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import io.devground.product.domain.exception.DomainException;
+import io.devground.product.domain.vo.DomainErrorCode;
+import io.devground.product.infrastructure.app.AppConfig;
+import io.devground.product.infrastructure.exception.InfraException;
+import io.devground.product.infrastructure.model.web.BaseResponse;
+import io.devground.product.infrastructure.vo.InfraErrorCode;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+@UtilityClass
+@Slf4j(topic = "service")
+public class LogUtil {
+
+	public void logControllerRequest(String className, String methodName) {
+
+		log.info("Request Controller = [{}.{}]", className, methodName);
+	}
+
+	public void logControllerResponse(
+		String className, String methodName, BaseResponse<?> baseResponse
+	) throws Throwable {
+
+		String jsonData = AppConfig.getObjectMapper().writeValueAsString(baseResponse.data());
+
+		log.info("Response Controller = [{}.{}], status: [{}], message: [{}], data: [{}]",
+			className, methodName, baseResponse.resultCode(), baseResponse.msg(), jsonData
+		);
+	}
+
+	public void logServiceRequest(String className, String methodName) {
+
+		log.info("Request Service = [{}.{}]", className, methodName);
+	}
+
+	public void logError(JoinPoint joinPoint, Throwable ex) {
+		String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+		String methodName = joinPoint.getSignature().getName();
+		String status;
+		String msg;
+
+		switch (ex) {
+			case DomainException exception -> {
+				DomainErrorCode errorCode = exception.getErrorCode();
+
+				status = String.valueOf(errorCode.getHttpStatus());
+				msg = errorCode.getMessage();
+			}
+			case InfraException exception -> {
+				InfraErrorCode errorCode = exception.getErrorCode();
+
+				status = String.valueOf(errorCode.getHttpStatus());
+				msg = errorCode.getMessage();
+			}
+			case MethodArgumentNotValidException exception -> {
+				status = HttpStatus.BAD_REQUEST.toString();
+				msg = exception.getMessage();
+			}
+			default -> {
+				status = "UNKNOWN";
+				msg = ex.getMessage();
+			}
+		}
+
+		log.error("ERROR = [{}.{}], status: [{}], message: [{}]",
+			className, methodName, status, msg
+		);
+	}
+}
