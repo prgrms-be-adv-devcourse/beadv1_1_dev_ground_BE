1a8b45860c7b5d91999b82f91b793c09428e0c90
GyeongHun <riverpower6@naver.com>
2025-12-01 17:26:51 +0900

    [FEAT] 기존 common에 구현해둔 aop, config, util, logback 이식

---
+++ product/src/main/java/io/devground/product/infrastructure/app/AppConfig.java
@@ -0,0 +1,20 @@
+package io.devground.product.infrastructure.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Getter;
+
+@Configuration
+public class AppConfig {
+
+	@Getter
+	private static ObjectMapper objectMapper;
+
+	@Autowired
+	public void setObjectMapper(ObjectMapper objectMapper) {
+		AppConfig.objectMapper = objectMapper;
+	}
+}
