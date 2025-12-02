c2c97609f0400a71ec6d4d95487eda3b98e41841
niki8533 <niki8533@naver.com>
2025-12-02 15:44:46 +0900

    [REFACTOR] user 정보 수정 시 valid 추가

--- user/src/main/java/com/example/user/model/dto/request/ModifyUserInfoRequest.java
+++ user/src/main/java/com/example/user/model/dto/request/ModifyUserInfoRequest.java
@@ -1,9 +1,16 @@
 package com.example.user.model.dto.request;
 
+import jakarta.validation.constraints.Pattern;
 
 public record ModifyUserInfoRequest(
 	String nickname,
+
+	@Pattern(
+		regexp = "^010-\\d{4}-\\d{4}$",
+		message = "전화번호 형식은 010-0000-0000입니다."
+	)
 	String phone,
+
 	String address,
 	String addressDetail
 ) {
