7db8e8763fbeacb9d4259ba71d8fdbb967e64d80
GyeongHun <riverpower6@naver.com>
2025-12-03 09:58:34 +0900

    [FEAT] infrastructure 껍데기 구현 및 application 단에서 사용할 Page 관련 클래스 구현 및 MethodArgumentResolver를 이용한 자동화
    1. ProductApiController 마이그레이션 및 필요 클래스들 마이그레이션
    2. PageDto 같은 application 단에서 사용할 Page 관련 객체 생성
    3. application 단에서 사용할 PageQuery를 컨트롤러에서 한 번에 받아오도록 구현
      - Layered Architecture에서는 컨트롤러에서 Pageable을 받아서 사용
      - 현재 구현하는 Hexagonal Architecture에서는 컨트롤러에서 domain단의 vo인 PageQuery를 한 번에 받아서 사용하도록 구현
      - MethodArgumentResolver 활용
      - Pageable -> PageQuery 컨버트를 별도로 하지 않아도 되도록 자동화 처리

---
+++ product/src/main/java/io/devground/product/infrastructure/adapter/in/web/config/PageQueryArgumentResolver.java
@@ -0,0 +1,82 @@
+package io.devground.product.infrastructure.adapter.in.web.config;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import io.devground.core.model.exception.ServiceException;
+import io.devground.core.model.vo.ErrorCode;
+import io.devground.product.domain.vo.pagination.PageQuery;
+import io.devground.product.domain.vo.pagination.SortSpec;
+
+@Component
+public class PageQueryArgumentResolver implements HandlerMethodArgumentResolver {
+
+	private static final int DEFAULT_PAGE = 1;
+	private static final int DEFAULT_SIZE = 10;
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+
+		return PageQuery.class.isAssignableFrom(parameter.getParameterType());
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+		String pageParam = webRequest.getParameter("page");
+		String sizeParam = webRequest.getParameter("size");
+		String sortParam = webRequest.getParameter("sort");
+
+		int page = convertToSafeParam(pageParam, DEFAULT_PAGE);
+		int size = convertToSafeParam(sizeParam, DEFAULT_SIZE);
+
+		SortSpec sortSpec = convertToSortSpec(sortParam);
+
+		return new PageQuery(page, size, sortSpec);
+	}
+
+	private int convertToSafeParam(String param, int defaultValue) {
+
+		try {
+			return Integer.parseInt(param);
+		} catch (NumberFormatException e) {
+			return defaultValue;
+		} catch (Exception e) {
+			throw new ServiceException(ErrorCode.PARAMETER_INVALID);
+		}
+	}
+
+	private SortSpec convertToSortSpec(String sortParam) {
+
+		if (!StringUtils.hasText(sortParam)) {
+			return null;
+		}
+
+		String[] sortBits = sortParam.split(",");
+
+		if (sortBits.length == 0 || sortBits[0].isBlank()) {
+			return null;
+		}
+
+		String property = sortBits[0];
+
+		SortSpec.Direction direction = null;
+		if (sortBits.length > 1) {
+			String sortDir = sortBits[1].trim();
+
+			if (sortDir.equalsIgnoreCase("asc")) {
+				direction = SortSpec.Direction.ASC;
+			} else {
+				direction = SortSpec.Direction.DESC;
+			}
+		}
+
+		return new SortSpec(property, direction);
+	}
+}
---
+++ product/src/main/java/io/devground/product/infrastructure/model/web/request/UpdateProductRequest.java
@@ -0,0 +1,28 @@
+package io.devground.product.infrastructure.model.web.request;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public record UpdateProductRequest(
+
+	@NotBlank(message = "상품명은 필수입니다.")
+	@Size(max = 100, message = "상품명은 100자까지만 가능합니다.")
+	String title,
+
+	@NotBlank(message = "상품 설명은 필수입니다.")
+	@Size(max = 3000, message = "상품 설명은 3000자까지만 가능합니다.")
+	String description,
+
+	@NotNull(message = "가격은 필수입니다.")
+	@Positive(message = "가격은 0원 이상이어야 합니다.")
+	Long price,
+
+	List<String> deleteUrls,
+
+	List<String> newImageExtensions
+) {
+}
