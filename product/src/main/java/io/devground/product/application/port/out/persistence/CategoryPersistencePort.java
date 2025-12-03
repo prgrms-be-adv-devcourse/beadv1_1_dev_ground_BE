53d91f9dab7e5ba227452475f9cdcdb049177b58
GyeongHun <riverpower6@naver.com>
2025-12-03 12:17:48 +0900

    [FEAT] Product 서비스 Root 카테고리 조회 기능 마이그레이션

--- product/src/main/java/io/devground/product/application/port/out/persistence/CategoryPersistencePort.java
+++ product/src/main/java/io/devground/product/application/port/out/persistence/CategoryPersistencePort.java
@@ -1,4 +1,10 @@
 package io.devground.product.application.port.out.persistence;
 
+import java.util.List;
+
+import io.devground.product.domain.vo.response.CategoryResponse;
+
 public interface CategoryPersistencePort {
+
+	List<CategoryResponse> findRootCategories();
 }
