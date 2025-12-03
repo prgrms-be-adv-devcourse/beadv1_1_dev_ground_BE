d2b3fb330f39524865758bd1b6f7f3ccdc728514
niki8533 <niki8533@naver.com>
2025-12-03 12:38:58 +0900

    [CHORE] 안쓰는 import 삭제

--- user/src/main/java/com/example/user/controller/UserController.java
+++ user/src/main/java/com/example/user/controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.user.controller;
 
 import java.io.IOException;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
