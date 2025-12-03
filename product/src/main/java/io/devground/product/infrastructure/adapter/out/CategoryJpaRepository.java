249c66f59c898784e7813e692d67852b34ebbd1d
GyeongHun <riverpower6@naver.com>
2025-12-03 12:17:48 +0900

    [FEAT] Product 서비스 Root 카테고리 조회 기능 마이그레이션

--- product/src/main/java/io/devground/product/infrastructure/adapter/out/CategoryJpaRepository.java
+++ product/src/main/java/io/devground/product/infrastructure/adapter/out/CategoryJpaRepository.java
@@ -1,8 +1,24 @@
 package io.devground.product.infrastructure.adapter.out;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import io.devground.product.infrastructure.model.persistence.CategoryEntity;
 
 public interface CategoryJpaRepository extends JpaRepository<CategoryEntity, Long> {
+
+	@Query("""
+		SELECT c
+		FROM CategoryEntity c
+		JOIN FETCH c.children
+		WHERE c.parent IS NULL
+		ORDER BY c.name ASC
+		""")
+	List<CategoryEntity> findCategoriesByParentIsNullOrderByNameAsc();
+
+	List<CategoryEntity> findCategoriesByParentIdOrderByNameAsc(Long parentId);
+
+	boolean existsCategoryById(Long id);
 }
--- product/src/main/java/io/devground/product/infrastructure/adapter/out/CategoryPersistenceAdapter.java
+++ product/src/main/java/io/devground/product/infrastructure/adapter/out/CategoryPersistenceAdapter.java
@@ -1,8 +1,12 @@
 package io.devground.product.infrastructure.adapter.out;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import io.devground.product.application.port.out.persistence.CategoryPersistencePort;
+import io.devground.product.domain.vo.response.CategoryResponse;
+import io.devground.product.infrastructure.model.persistence.CategoryEntity;
 import lombok.RequiredArgsConstructor;
 
 @Repository
@@ -11,5 +15,13 @@ public class CategoryPersistenceAdapter implements CategoryPersistencePort {
 
 	private final CategoryJpaRepository categoryRepository;
 
+	@Override
+	public List<CategoryResponse> findRootCategories() {
 
+		List<CategoryEntity> rootCategories = categoryRepository.findCategoriesByParentIsNullOrderByNameAsc();
+
+		return rootCategories.stream()
+			.map(CategoryResponse::new)
+			.toList();
+	}
 }
