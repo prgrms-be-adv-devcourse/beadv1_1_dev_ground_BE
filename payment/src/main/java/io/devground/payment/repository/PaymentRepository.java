efe1b9b10a7b16013b8fb93f505b8a261544aa35
niki8533 <niki8533@naver.com>
2025-12-04 16:10:31 +0900

    [FEAT] 전체 결제 내역 조회 api 추가

--- payment/src/main/java/io/devground/payment/repository/PaymentRepository.java
+++ payment/src/main/java/io/devground/payment/repository/PaymentRepository.java
@@ -2,12 +2,17 @@ package io.devground.payment.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import io.devground.core.model.vo.DeleteStatus;
 import io.devground.payment.model.entity.Payment;
 import io.devground.payment.model.vo.PaymentStatus;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 	Optional<Payment> findByOrderCode(String orderCode);
 	Optional<Payment> findByUserCodeAndPaymentStatus(String userCode, PaymentStatus paymentStatus);
+
+	Page<Payment> findByUserCodeByCreatedAtDesc(String userCode, Pageable pageable);
 }
