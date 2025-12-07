3bf946d9401fd56d07e66566894cd6406e06aaf7
Gyeonghun Min <riverpower6@naver.com>
2025-12-07 21:01:56 +0900

    [CHORE] ApplicationImageType을 제거하고 core의 ImageType을 사용하도록 수정

--- product/src/main/java/io/devground/product/application/port/out/ImageClientPort.java
+++ product/src/main/java/io/devground/product/application/port/out/ImageClientPort.java
@@ -4,11 +4,10 @@ import java.net.URL;
 import java.util.List;
 
 import io.devground.core.model.vo.ImageType;
-import io.devground.product.application.model.vo.ApplicationImageType;
 
 public interface ImageClientPort {
 
-	List<String> getImageUrls(String productCode, ApplicationImageType imageType);
+	List<String> getImageUrls(String productCode, ImageType imageType);
 
 	List<URL> prepareUploadUrls(ImageType imageType, String productCode, List<String> fileExtensions);
 
