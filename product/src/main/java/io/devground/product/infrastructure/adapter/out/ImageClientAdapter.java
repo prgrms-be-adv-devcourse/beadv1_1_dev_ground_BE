275dbb17424d8c7a6ee166dade6ddebde70ec2bd
Gyeonghun Min <riverpower6@naver.com>
2025-12-07 15:10:27 +0900

    [CHORE] Product에서 사용하는 ImagePersistencePort 이름을 ImageClientPort로 변경

--- product/src/main/java/io/devground/product/infrastructure/adapter/out/ImageClientAdapter.java
+++ product/src/main/java/io/devground/product/infrastructure/adapter/out/ImageClientAdapter.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import io.devground.core.model.vo.ImageType;
 import io.devground.core.model.web.BaseResponse;
 import io.devground.product.application.model.vo.ApplicationImageType;
-import io.devground.product.application.port.out.ImagePersistencePort;
+import io.devground.product.application.port.out.ImageClientPort;
 import io.devground.product.infrastructure.adapter.out.client.ImageClient;
 import io.devground.product.infrastructure.model.web.request.ImageUpdatePlan;
 import io.devground.product.infrastructure.model.web.request.ImageUploadPlan;
@@ -16,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class ImagePersistenceAdapter implements ImagePersistencePort {
+public class ImageClientAdapter implements ImageClientPort {
 
 	private final ImageClient imageClient;
 
