4d62213c5264e8bfdff12b98b71289241acbcdf2
Gyeonghun Min <riverpower6@naver.com>
2025-12-07 22:23:38 +0900

    [FEAT] Domain과의 작업을 위해 ImageMapper 내부에 메서드 추가

--- image/src/main/java/io/devground/image/infrastructure/mapper/ImageMapper.java
+++ image/src/main/java/io/devground/image/infrastructure/mapper/ImageMapper.java
@@ -1,6 +1,8 @@
 package io.devground.image.infrastructure.mapper;
 
 import io.devground.core.model.vo.ImageType;
+import io.devground.image.domain.model.Image;
+import io.devground.image.domain.vo.ImageSpec;
 import io.devground.image.infrastructure.model.persistence.ImageEntity;
 import lombok.experimental.UtilityClass;
 
@@ -14,5 +16,23 @@ public class ImageMapper {
 			.referenceCode(referenceCode)
 			.imageUrl(imageUrls)
 			.build();
+	}
+
+	public Image toImageDomain(ImageEntity imageEntity) {
+
+		ImageSpec imageSpec = new ImageSpec(
+			imageEntity.getReferenceCode(),
+			imageEntity.getImageType(),
+			imageEntity.getImageUrl()
+		);
+
+		return new Image(imageSpec);
+	}
+
+	public ImageEntity toImageEntity(Image image) {
+
+		ImageSpec imageSpec = image.getImageSpec();
+
+		return of(imageSpec.imageType(), imageSpec.referenceCode(), imageSpec.imageUrl());
 	}
 }
