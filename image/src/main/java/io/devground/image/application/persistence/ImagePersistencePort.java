c71f4e09975605a2ae10bb60feb4c714a6bd6166
Gyeonghun Min <riverpower6@naver.com>
2025-12-07 19:08:07 +0900

    [FEAT] Image 모듈 이미지 저장 구현

--- image/src/main/java/io/devground/image/application/persistence/ImagePersistencePort.java
+++ image/src/main/java/io/devground/image/application/persistence/ImagePersistencePort.java
@@ -8,4 +8,8 @@ import io.devground.core.model.vo.ImageType;
 public interface ImagePersistencePort {
 
 	List<URL> generatePresignedUrls(ImageType imageType, String referenceCode, List<String> fileExtensions);
+
+	List<String> getImageUrls(ImageType imageType, String referenceCode);
+
+	void saveImages(ImageType imageType, String referenceCode, List<String> newUrls);
 }
