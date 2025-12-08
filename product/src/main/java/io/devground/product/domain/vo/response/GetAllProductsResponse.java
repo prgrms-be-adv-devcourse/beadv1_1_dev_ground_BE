d253802fe5abb637e75c13b2ddc692d86952483b
GyeongHun <riverpower6@naver.com>
2025-12-08 10:16:24 +0900

    [REFACTOR] 상품 목록 조회 시 상품 판매 상태 함께 반환

--- product/src/main/java/io/devground/product/domain/vo/response/GetAllProductsResponse.java
+++ product/src/main/java/io/devground/product/domain/vo/response/GetAllProductsResponse.java
@@ -8,6 +8,7 @@ public record GetAllProductsResponse(
 	String productCode,
 	String title,
 	String thumbnailUrl,
+	String productStatus,
 	long price
 ) {
 	public GetAllProductsResponse(ProductEntity product, ProductSaleEntity productSale) {
@@ -15,50 +16,8 @@ public record GetAllProductsResponse(
 			product.getCode(),
 			product.getTitle(),
 			product.getThumbnailUrl(),
+			productSale.getProductStatus().getValue(),
 			productSale.getPrice()
 		);
-	}
-
-	public static GetAllProductsResponseBuilder builder() {
-		return new GetAllProductsResponseBuilder();
-	}
-
-	public static class GetAllProductsResponseBuilder {
-		private String productCode;
-		private String title;
-		private String thumbnailUrl;
-		private long price;
-
-		GetAllProductsResponseBuilder() {
-		}
-
-		public GetAllProductsResponseBuilder productCode(String productCode) {
-			this.productCode = productCode;
-			return this;
-		}
-
-		public GetAllProductsResponseBuilder title(String title) {
-			this.title = title;
-			return this;
-		}
-
-		public GetAllProductsResponseBuilder thumbnailUrl(String thumbnailUrl) {
-			this.thumbnailUrl = thumbnailUrl;
-			return this;
-		}
-
-		public GetAllProductsResponseBuilder price(long price) {
-			this.price = price;
-			return this;
-		}
-
-		public GetAllProductsResponse build() {
-			return new GetAllProductsResponse(this.productCode, this.title, this.thumbnailUrl, this.price);
-		}
-
-		public String toString() {
-			return "GetAllProductsResponse.GetAllProductsResponseBuilder(productCode=" + this.productCode + ", title="
-				+ this.title + ", thumbnailUrl=" + this.thumbnailUrl + ", price=" + this.price + ")";
-		}
 	}
 }
