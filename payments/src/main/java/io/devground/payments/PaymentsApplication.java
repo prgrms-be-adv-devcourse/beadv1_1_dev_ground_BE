e92f832c5216b60fa605e45d5ff268ed1969ec6c
niki8533 <niki8533@naver.com>
2025-12-08 17:19:44 +0900

    [REFACTOR] payments yml에 payment 모듈 이동

--- payments/src/main/java/io/devground/payments/PaymentsApplication.java
+++ payments/src/main/java/io/devground/payments/PaymentsApplication.java
@@ -2,7 +2,21 @@ package io.devground.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+	servers = {
+		@Server(url = "/dbay", description = "Dbay service via gateway")
+	}
+)
+@EnableAsync
+@EnableKafka
+@EnableFeignClients
 @SpringBootApplication
 public class PaymentsApplication {
 
