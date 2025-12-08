858a8a171ddaa580a363334ba363f160fc241195
Jaeseok Lee <ljs981026@naver.com>
2025-12-08 11:31:20 +0900

    [REFACTOR] 주문 확정 기능 헥사고날 아키텍처 적용

--- commerce/src/main/java/io/devground/dbay/order/infrastructure/adapter/out/persistence/OrderJpaRepository.java
+++ commerce/src/main/java/io/devground/dbay/order/infrastructure/adapter/out/persistence/OrderJpaRepository.java
@@ -4,13 +4,23 @@ import io.devground.dbay.order.infrastructure.model.persistence.OrderEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface OrderJpaRepository extends JpaRepository<OrderEntity, Long> {
     Optional<OrderEntity> findByCode(String orderCode);
+
+    @Query("""
+        SELECT o.updatedAt
+        FROM OrderEntity o
+        WHERE o.code = :orderCode
+        """)
+    LocalDateTime findUpdatedAtByCode(String orderCode);
+
     @Query("""
         SELECT o.id
         FROM OrderEntity o
@@ -33,11 +43,21 @@ public interface OrderJpaRepository extends JpaRepository<OrderEntity, Long> {
         """)
     Page<OrderEntity> findAllByNotDeletedOrders(Pageable pageable);
 
+    @Modifying
     @Query("""
         UPDATE OrderEntity o
-        SET o.orderStatus = io.devground.dbay.order.domain.vo.OrderStatus.CANCELLED
+        SET o.orderStatus = io.devground.dbay.order.domain.vo.OrderStatus.CANCELLED,
+        o.updatedAt = CURRENT_TIMESTAMP
         WHERE o.code = :orderCode
         """)
     void cancelByCode(String orderCode);
 
+    @Modifying
+    @Query("""
+        UPDATE OrderEntity o
+        SET o.orderStatus = io.devground.dbay.order.domain.vo.OrderStatus.CONFIRMED,
+        o.updatedAt = CURRENT_TIMESTAMP
+        WHERE o.code = :orderCode
+        """)
+    void confirmByCode(String orderCode);
 }
