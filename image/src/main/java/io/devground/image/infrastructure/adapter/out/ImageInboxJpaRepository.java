1a10b086ee48ca0fff3eeaeb75126a9925f3a0de
GyeongHun <riverpower6@naver.com>
2025-12-09 17:24:24 +0900

    [CHORE] Product 모듈에 Image 모듈 코드 병합으로 인한 Image 모듈 삭제

--- image/src/main/java/io/devground/image/infrastructure/adapter/out/ImageInboxJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package io.devground.image.infrastructure.adapter.out;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.devground.image.infrastructure.model.persistence.ImageInbox;
-
-public interface ImageInboxJpaRepository extends JpaRepository<ImageInbox, Long> {
-}
