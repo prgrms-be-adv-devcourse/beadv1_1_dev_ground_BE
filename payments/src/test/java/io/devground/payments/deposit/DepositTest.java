289234f5013325a9d9c1e33d7796bfc297018d4a
kurt <qodydgus97@kakao.com>
2025-12-10 16:47:36 +0900

    [FEAT] deposit 도메인 테스트 코드 추가

--- payments/src/test/java/io/devground/payments/deposit/DepositTest.java
+++ payments/src/test/java/io/devground/payments/deposit/DepositTest.java
@@ -1,10 +1,13 @@
 package io.devground.payments.deposit;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import io.devground.payments.deposit.domain.deposit.Deposit;
+import io.devground.payments.deposit.domain.exception.DomainException;
 
 @DisplayName("도메인 - Deposit")
 class DepositTest {
@@ -18,21 +21,30 @@ class DepositTest {
 		void givenValidUserCode_whenCreating_thenCreatesDeposit() {
 			// given
 			String userCode = "USER_CODE";
-			new Deposit(userCode);
 
 			// when
+			Deposit deposit = new Deposit(userCode);
 
 			// then
+			assertThat(deposit)
+				.isNotNull()
+				.returns(userCode, Deposit::getUserCode)
+				.extracting(Deposit::getCode, Deposit::getCreatedAt, Deposit::getUpdatedAt)
+				.doesNotContainNull();
 		}
 
 		@DisplayName("초기 잔액은 0원이다.")
 		@Test
 		void givenNewDeposit_whenCreated_thenBalanceIsZero() {
 			// given
+			String userCode = "USER_CODE";
 
 			// when
+			Deposit deposit = new Deposit(userCode);
 
 			// then
+			assertThat(deposit.getBalance())
+				.isZero();
 		}
 	}
 
@@ -44,34 +56,48 @@ class DepositTest {
 		@Test
 		void givenPositiveAmount_whenCharging_thenBalanceIncreases() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
+			Long chargeAmount = 10000L;
 
 			// when
+			deposit.charge(chargeAmount);
 
 			// then
+			assertThat(deposit.getBalance())
+				.isEqualTo(10000L);
 		}
 
 		@DisplayName("0원을 입력하면, 예외를 발생시킨다.")
 		@Test
 		void givenZeroAmount_whenCharging_thenThrowsException() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
 
 			// when & then
+			assertThatThrownBy(() -> deposit.charge(0L))
+				.isInstanceOf(DomainException.class);
 		}
 
 		@DisplayName("음수 금액을 입력하면, 예외를 발생시킨다.")
 		@Test
 		void givenNegativeAmount_whenCharging_thenThrowsException() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
 
 			// when & then
+			assertThatThrownBy(() -> deposit.charge(-1000L))
+				.isInstanceOf(DomainException.class);
 		}
 
 		@DisplayName("null을 입력하면, 예외를 발생시킨다.")
 		@Test
 		void givenNullAmount_whenCharging_thenThrowsException() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
 
 			// when & then
+			assertThatThrownBy(() -> deposit.charge(null))
+				.isInstanceOf(DomainException.class);
 		}
 	}
 
@@ -83,42 +109,64 @@ class DepositTest {
 		@Test
 		void givenSufficientBalance_whenWithdrawing_thenSucceeds() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
+			deposit.charge(10000L);
+			Long withdrawAmount = 5000L;
 
 			// when
+			deposit.withdraw(withdrawAmount);
 
 			// then
+			assertThat(deposit.getBalance())
+				.isEqualTo(5000L);
 		}
 
 		@DisplayName("잔액이 부족하면, 예외를 발생시킨다.")
 		@Test
 		void givenInsufficientBalance_whenWithdrawing_thenThrowsException() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
+			deposit.charge(5000L);
 
 			// when & then
+			assertThatThrownBy(() -> deposit.withdraw(10000L))
+				.isInstanceOf(DomainException.class);
 		}
 
 		@DisplayName("0원을 입력하면, 예외를 발생시킨다.")
 		@Test
 		void givenZeroAmount_whenWithdrawing_thenThrowsException() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
+			deposit.charge(10000L);
 
 			// when & then
+			assertThatThrownBy(() -> deposit.withdraw(0L))
+				.isInstanceOf(DomainException.class);
 		}
 
 		@DisplayName("음수 금액을 입력하면, 예외를 발생시킨다.")
 		@Test
 		void givenNegativeAmount_whenWithdrawing_thenThrowsException() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
+			deposit.charge(10000L);
 
 			// when & then
+			assertThatThrownBy(() -> deposit.withdraw(-1000L))
+				.isInstanceOf(DomainException.class);
 		}
 
 		@DisplayName("null을 입력하면, 예외를 발생시킨다.")
 		@Test
 		void givenNullAmount_whenWithdrawing_thenThrowsException() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
+			deposit.charge(10000L);
 
 			// when & then
+			assertThatThrownBy(() -> deposit.withdraw(null))
+				.isInstanceOf(DomainException.class);
 		}
 	}
 
@@ -130,30 +178,45 @@ class DepositTest {
 		@Test
 		void whenGettingCode_thenReturnsCode() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
 
 			// when
+			String code = deposit.getCode();
 
 			// then
+			assertThat(code)
+				.isNotNull()
+				.isNotEmpty();
 		}
 
 		@DisplayName("사용자 코드를 반환한다.")
 		@Test
 		void whenGettingUserCode_thenReturnsUserCode() {
 			// given
+			String expectedUserCode = "USER_CODE";
+			Deposit deposit = new Deposit(expectedUserCode);
 
 			// when
+			String userCode = deposit.getUserCode();
 
 			// then
+			assertThat(userCode)
+				.isEqualTo(expectedUserCode);
 		}
 
 		@DisplayName("잔액을 반환한다.")
 		@Test
 		void whenGettingBalance_thenReturnsBalance() {
 			// given
+			Deposit deposit = new Deposit("USER_CODE");
+			deposit.charge(15000L);
 
 			// when
+			Long balance = deposit.getBalance();
 
 			// then
+			assertThat(balance)
+				.isEqualTo(15000L);
 		}
 	}
 
