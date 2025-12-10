1a3ea9584b7b361c2d1470efcc48799113aa41cd
kurt <qodydgus97@kakao.com>
2025-12-10 16:31:41 +0900

    [REFACTOR] deposit 테스트 코드 이전

--- core/src/main/java/io/devground/core/util/feign/FeignConfig.java
+++ core/src/main/java/io/devground/core/util/feign/FeignConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import feign.codec.ErrorDecoder;
 
@@ -12,7 +14,11 @@ public class FeignConfig {
 
 	@Bean
 	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+		return objectMapper;
 	}
 
 	@Bean
