6434b5f4ddfca4cdaa72582daafadf83c3132b6c
jihei <han01031@naver.com>
2025-12-12 14:27:37 +0900

    [FEAT] chatRoom 생성 로직

---
+++ chat/src/main/java/com/example/chat/controller/ChatController.java
@@ -0,0 +1,42 @@
+package com.example.chat.controller;
+
+import com.example.chat.client.ProductClient;
+import com.example.chat.client.UserClient;
+import com.example.chat.enums.ChatRoomStatus;
+import com.example.chat.model.dto.request.ChatRoomRequest;
+import com.example.chat.model.dto.response.ChatRoomSummary;
+import com.example.chat.model.entity.ChatMessages;
+import com.example.chat.model.entity.ChatRoom;
+import com.example.chat.service.ChatMessageService;
+import com.example.chat.service.ChatRoomService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Locale;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat")
+@Tag(name = "ChatController")
+
+public class ChatController {
+
+      private final ChatRoomService chatRoomService;
+
+
+    //채팅방 생성
+    @PostMapping("/rooms")
+    public ChatRoom createOrGetRoom(@RequestBody ChatRoomRequest request) {
+        return chatRoomService.getOrCreateRoom(
+                request.getProductCode(),
+                request.getSellerCode(),
+                request.getBuyerCode()
+        );
+    }
+
+
+}
