606a910d552e267e849a5ee18f6fae7ad4520abe
jihei <han01031@naver.com>
2025-12-12 14:16:29 +0900

    [FEAT] chat entity, dto 작성

---
+++ chat/src/main/java/com/example/chat/model/dto/response/UserInfoResponse.java
@@ -0,0 +1,11 @@
+package com.example.chat.model.dto.response;
+
+public record UserInfoResponse(
+        String name,
+        String email,
+        String nickname,
+        String phone,
+        String address,
+        String addressDetail
+) {
+}
