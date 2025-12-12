c6e21e6fe315dedd25609afe44416d20251f719f
jihei <han01031@naver.com>
2025-12-12 14:13:52 +0900

    [FEAT] chat yml설정

---
+++ chat/src/main/java/com/example/chat/ChatApplication.java
@@ -0,0 +1,17 @@
+package com.example.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@SpringBootApplication
+@EnableKafka
+@EnableFeignClients
+public class ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+
+}
