691be16596d84992ed2598852e8f25807e5b5996
Jaeseok Lee <ljs981026@naver.com>
2025-12-16 17:49:50 +0900

    [FEAT] 장바구니 프론트 요구에 맞게 수정2

--- commerce/src/main/java/io/devground/dbay/cart/application/port/out/product/CartProductPort.java
+++ commerce/src/main/java/io/devground/dbay/cart/application/port/out/product/CartProductPort.java
@@ -3,11 +3,12 @@ package io.devground.dbay.cart.application.port.out.product;
 import io.devground.dbay.cart.application.vo.ProductInfoSnapShot;
 import io.devground.dbay.cart.application.vo.ProductSnapShot;
 import io.devground.dbay.cart.domain.vo.ProductCode;
+import io.devground.dbay.cart.domain.vo.UserCode;
 
 import java.util.List;
 
 public interface CartProductPort {
-    ProductSnapShot getProduct(ProductCode productCode);
+    ProductSnapShot getProduct(UserCode userCode, ProductCode productCode);
 
     List<ProductInfoSnapShot> getCartProducts(List<ProductCode> productCodes);
 }
