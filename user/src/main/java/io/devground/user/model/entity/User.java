ebe4ec4d378ed972df5303c480bd8e537ec0ae74
Jaeseok Lee <ljs981026@naver.com>
2025-12-18 09:26:15 +0900

    [REFACTOR] User엔티티 email 필드 unique 옵션 추가

--- user/src/main/java/io/devground/user/model/entity/User.java
+++ user/src/main/java/io/devground/user/model/entity/User.java
@@ -34,7 +34,7 @@ public class User extends BaseEntity {
 	@Column(nullable = false)
 	private String password;
 
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String email;
 
 	@Setter
